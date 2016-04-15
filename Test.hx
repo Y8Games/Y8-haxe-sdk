de0ac84d2407754342404d477b1b8a5745609dd2
Codir <codir2@gmail.com>
2016-04-15 21:08:45 +0300

    add examples and comments

--- Test.hx
+++ Test.hx
@@ -17,28 +17,39 @@ class Test extends Sprite
 	public function new() 
 	{
 		super();
-		Social.get_i().init("55af9f5b7765623f71000433", "9129ecc6335935a4350f84fab7fdfcd9acf15e56398678ee77d77e1e03a0d6e2");
-		Social.get_i().addEventListener(IDNetEvent.ID_INITIALIZE_COMPLETE, onIDInitComplete);
-		Social.get_i().addEventListener(IDNetEvent.ID_INITIALIZE_FAILED, onIDInitFailed);
-		Social.get_i().addEventListener(IDNetEvent.ID_AUTH_COMPLETE, onIDAuthComplete);
-		Social.get_i().addEventListener(IDNetEvent.ID_BLACKLISTED, onIDBlacklisted);
-		Social.get_i().addEventListener(IDNetEvent.IS_SPONSOR, onIDSponsor);
-		Social.get_i().addEventListener(IDNetEvent.ACHIEVEMENT_UNLOCKED, onIDAchiviementUnlocked);
-	}
+		Social.get_i().init("55af9f5b7765623f71000433", "9129ecc6335935a4350f84fab7fdfcd9acf15e56398678ee77d77e1e03a0d6e2"); //Initialization app with appKey and appSecret from https://www.id.net/applications/
+		Social.get_i().addEventListener(IDNetEvent.ID_INITIALIZE_COMPLETE, onIDInitComplete); //Callback on successful initialization
+		Social.get_i().addEventListener(IDNetEvent.ID_INITIALIZE_FAILED, onIDInitFailed); //Callback on failed initialization
+		
+		//--------Authorization--------//
+		Social.get_i().addEventListener(IDNetEvent.ID_AUTH_COMPLETE, onIDAuthComplete); //Callback calling, when authorization is complete successfully
+		
+		Social.get_i().loginPopup(); //Show IDNet UI - Login popup
+		Social.get_i().register(); //Show IDNet UI - Register popup
+		
+		Social.get_i().get_username(); //Return username {String}
+		
+		Social.get_i().isAuthorized(); //Return true if, user authorized {Bool}
+		Social.get_i().logout(); //Logout
+		
+		//--------Protection API--------//
+		Social.get_i().addEventListener(IDNetEvent.ID_BLACKLISTED, onIDBlacklisted); //Callback calling, if domain in blacklist
+		Social.get_i().addEventListener(IDNetEvent.IS_SPONSOR, onIDSponsor); //Callback calling, if domain in sponsor list
+		
+		//--------Leaderboards--------//
+		Social.get_i().submitScore("Leaderboard", 1, Social.get_i().get_username()); //Submit score to leaderboard with "Leaderboard" name from https://www.id.net/applications/
+		Social.get_i().showLeaderBoard("Leaderboard"); //Show IDNet UI - Leaderboard with "Leaderboard" name from https://www.id.net/applications/
 	
-	private function onIDAchiviementUnlocked(e:Event):Void 
-	{
-		trace("IDNetEvent.ACHIEVEMENT_UNLOCKED");
-	}
-	
-	private function onIDSponsor(e:Event):Void 
-	{
-		trace("Domain in sponsor list");
-	}
-	
-	private function onIDBlacklisted(e:Event):Void 
-	{
-		trace("Domain in blacklist");
+		//--------Cloud saves--------//
+		Social.get_i().setSaveData("field", "value"); //Save data "value" to cloud saves with "field" name
+		Social.get_i().getSaveData("field", function(responce:Dynamic) { } ); //Return data from cloud saves with "field" name
+		Social.get_i().clearSaveData("field"); //Clear data in cloud saves with "field" name
+		
+		//--------Achievements--------//
+		Social.get_i().achievementsSave("achievementName", "achievementKey", Social.get_i().get_username()); //Unlock achievement with "achievementName" name and "achievementKey" key
+		Social.get_i().addEventListener(IDNetEvent.ACHIEVEMENT_UNLOCKED, onIDAchiviementUnlocked); //Callback calling, after unlocking achievement
+		
+		Social.get_i().achievementsList(); //Show IDNet UI - Achievements list
 	}
 	
 	private function onIDAuthComplete(e:Event):Void 
@@ -57,5 +68,18 @@ class Test extends Sprite
 		trace("IDNetEvent.ID_INITIALIZE_COMPLETE");
 	}
 	
-	//Leaderboard
+	private function onIDSponsor(e:Event):Void 
+	{
+		trace("Domain in sponsor list");
+	}
+	
+	private function onIDBlacklisted(e:Event):Void 
+	{
+		trace("Domain in blacklist");
+	}
+
+	private function onIDAchiviementUnlocked(e:Event):Void 
+	{
+		trace("IDNetEvent.ACHIEVEMENT_UNLOCKED");
+	}
 }
